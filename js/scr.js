773efce23cce53bc2e19ec89ab324e216eeba7de
vintr <dev1ce@icloud.com>
2018-09-24 01:27:34 +0300

    Fixed

--- js/scr.js
+++ js/scr.js
@@ -38,6 +38,7 @@ function ifHaveAccount() {
     var y = document.getElementById("ntr-create-account");
     if (x.style.display === "none") {
         x.style.display = "block";
+        y.style.display = "none";
     } else {
         x.style.display = "none";
         y.style.display = "block";
